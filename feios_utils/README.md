1b981a64ab050e9100bd80c480f328232197c1f9
devoter-fyc <104010629+devoter-fyc@users.noreply.github.com>
2022-05-16 09:45:28 +0800

    Add files via upload

--- feios_utils/README.md
+++ feios_utils/README.md
@@ -1,2 +1,21 @@
-* New implementation for feios_funcs.
-* Avalible through pip.
+### FEIOS_UTILS
+* This is a library for feios to run.
+* This includes some small and useful utils.
+* You can use it in your app like:
+```python
+import feios_utils
+feios_utils.out("Hello")
+```
+* Note that the library is licensed under GNU GPL v3+.Your app should be licensed under that too.
+
+====================
+
+### Changelog 
+#### Changelog @ 0.2.0a2
+* Fixed corrupt IndexError in funcs.run.
+#### Changelog @ 0.2.0a1
+* no change (huh)
+#### Changelog @ 0.2.0.dev3
+* Added support back to py3.5.
+#### Changelog @ 0.2.0.dev2
+* Added load_cmd for **later** use.
---
+++ feios_utils/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="feios-utils",
+    version="0.2.0.a2",
+    author="fyc",
+    author_email="fycsfls_winter@126.com",
+    description="A small util package for feios",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/devoter-fyc/fei-ros",
+    project_urls={
+        "Bug Tracker": "https://github.com/devoter-fyc/fei-ros/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.5",
+)
