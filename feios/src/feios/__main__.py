28f16c314dc7b71bc30dcc52814e90bedddb136d
devoter-fyc <104010629+devoter-fyc@users.noreply.github.com>
2022-05-17 19:02:18 +0800

    Add files via upload

---
+++ feios/src/feios/__main__.py
@@ -0,0 +1,2 @@
+import subprocess
+subprocess.call("py feios.py")
---
+++ feios/src/feios/feios.py
@@ -0,0 +1,35 @@
+import sys,os,time
+import feios_utils.funcs as utils
+
+################################
+def load_cmd(cmd : str):
+    """Load a cmd."""
+    if cmd[0] == "#":
+        return
+    elif cmd[:3] == "out":
+        if cmd[3] == " ":
+            print(cmd[4:])
+        else:
+            print("""Input error:               ERROR 001
+            There should be a space.
+            Execution halted.
+            """)
+            return
+    elif cmd[:4] == "exit":
+        if len(cmd) >= 5:
+            exit(cmd[5:])
+        else:
+            exit()
+    elif cmd == "":
+        return
+    else:
+        print("""Input error:                   ERROR 002
+        No such command.
+        Execution halted.
+        """)
+        return
+    return
+
+while True:
+    s = input(">>>")
+    load_cmd(s)
